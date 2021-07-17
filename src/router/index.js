f6c554547c3d2d6e488c3bb3080834c427c86efa
Yavuz Selim <selimserifoglu03@gmail.com>
2021-07-17 15:03:36 +0300

    tailwind added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Test from '../views/Test.vue'
 
 const routes = [
   {
@@ -14,12 +15,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: Test
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router
---
+++ src/views/Test.vue
@@ -0,0 +1,10 @@
+<template lang="">
+    <div>
+        Test
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="">
+</style>
